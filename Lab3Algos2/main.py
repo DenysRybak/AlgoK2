8232b3ce6f5403e9316ecbfc6c58b5ec8385183c
DenysRybak <mribak1983@gmail.com>
2023-10-27 00:23:09 +0300

    remastered lab3

--- Lab3Algos2/main.py
+++ Lab3Algos2/main.py
@@ -54,9 +54,37 @@ if __name__ == "__main__":
 import unittest
 
 class TestBinaryTreeFunctions(unittest.TestCase):
+    # Тест на те чи правильно розвертається дерево
     def test_invert_binary_tree(self):
         # Тест на порожньому дереві
         self.assertIsNone(invert_binary_tree(None))
+
+        # Тест на одному вузлі
+        tree = BinaryTree(1)
+        inverted_tree = invert_binary_tree(tree)
+        self.assertEqual(inverted_tree.value, 1)
+        self.assertIsNone(inverted_tree.left)
+        self.assertIsNone(inverted_tree.right)
+
+        # Тест на дереві з багатьма вузлами
+        root = BinaryTree(1)
+        root.left = BinaryTree(2)
+        root.right = BinaryTree(3)
+        root.left.left = BinaryTree(4)
+        root.left.right = BinaryTree(5)
+        root.right.left = BinaryTree(6)
+        root.right.right = BinaryTree(7)
+
+        inverted_root = invert_binary_tree(root)
+
+        # Перевірка дзеркального дерева
+        self.assertEqual(inverted_root.value, 1)
+        self.assertEqual(inverted_root.left.value, 3)
+        self.assertEqual(inverted_root.right.value, 2)
+        self.assertEqual(inverted_root.left.left.value, 7)
+        self.assertEqual(inverted_root.left.right.value, 6)
+        self.assertEqual(inverted_root.right.left.value, 5)
+        self.assertEqual(inverted_root.right.right.value, 4)
 
     def test_in_order_traversal(self):
         # Тест на порожньому дереві
